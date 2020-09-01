9183a565b7902bc9e64fe1469dfad93fdb01eccb
Slawek Walkowski <swalk@google.com>
2020-09-01 14:40:05 -0400

    Demo DAG

---
+++ hello-world-demo.py
@@ -0,0 +1,21 @@
+import airflow
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import timedelta
+
+default_args = {
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'start_date': airflow.utils.dates.days_ago(0)
+}
+
+
+dag = DAG(
+    'hello-world-demo',
+    default_args=default_args,
+    description='git-sync testing',
+    schedule_interval=timedelta(minutes=5))
+
+
+t1 = BashOperator(
+    task_id='echo', bash_command='echo welcome!', dag=dag, depends_on_past=False)
